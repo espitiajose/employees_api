8444932ecf079fa65b334593654cff196eead25b
Jose Espitia <jcpe1610@gmail.com>
2021-06-26 11:32:47 -0500

    production config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    isProducction: process.env.NODE_ENV == 'prod',
+    isProducction: process.env.NODE_ENV != 'dev',
     mongobd: process.env.MONGOBD_CNN,
     port: process.env.PORT,
   };
